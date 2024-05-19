2f881610187ddcf6ddd36133db53d4eddc2810e3
Bryl <150446869+BrylAce@users.noreply.github.com>
2024-05-19 21:15:44 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -53,7 +53,4 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
-<?php
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
-?>
+
